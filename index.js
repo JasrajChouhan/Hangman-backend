3a0565732344ed45a6c273d704765f71cd227fa4
JasrajChouhan <jasrajc945@gmail.com>
2024-09-15 15:44:18 +0530

    Add FRONTEND_URL_PLAY in cors

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 const frontendUrl = process.env.FRONTEND_URL || ""
+const frontendUrlPlay = process.env.FRONTEND_URL_PLAY ;
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 app.use(cors({
-    origin: isProduction ? frontendUrl : ['http://localhost:5173/' , 'http://localhost:5173/play']
+    origin: isProduction ? [frontendUrl , frontendUrlPlay] : ['http://localhost:5173/' , 'http://localhost:5173/play']
 }));
 
 app.get('/', (req, res) => {
